1c2aa5d30ad5ffee3b4f83a8483818216e7c70cc
Javin Raj Abraham <107456532+Javin3235@users.noreply.github.com>
2023-05-31 13:22:20 +0530

    Add files via upload
    initial commit.

---
+++ css/temperature_con.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+	public class temperature_con{
+		public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		double temperature;
+		int choice;
+		while(true) {
+			system.out.println("choose conversion type :")	;
+			system.out.println("1. Celsius to Farenheit");
+			system.out.println("2. Farenheit to celsius");
+			system.out.println("3. Celsius to Kelvin");
+			system.out.println("4. Kelvin to Celsius");
+			system.out.println("5. Kelvin to Farenheit");
+			system.out.println("6. Farenheit to Kelvin");
+			system.out.println("Exit");
+				choice = sc.nextInt();
+	
